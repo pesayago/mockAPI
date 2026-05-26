fda1f01826a57bfb823abbb868ad8390ee2d8f41
Pablo Sayago <psayago@frba.utn.edu.ar>
2026-05-26 16:09:08 -0300

    primer commit

---
+++ src/main/java/com/example/pruebaApp/JacksonConfiguration.java
@@ -0,0 +1,34 @@
+package com.example.pruebaApp;
+
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.format.DateTimeFormatter;
+
+@Configuration
+public class JacksonConfiguration {
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+        
+        return builder -> {
+            
+            // formatter
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            DateTimeFormatter dateTimeFormatter =  DateTimeFormatter.ofPattern("HH:mm dd/MM/yyyy");
+            
+            // deserializers
+            builder.deserializers(new LocalDateDeserializer(dateFormatter));
+            builder.deserializers(new LocalDateTimeDeserializer(dateTimeFormatter));
+            
+            // serializers
+            builder.serializers(new LocalDateSerializer(dateFormatter));
+            builder.serializers(new LocalDateTimeSerializer(dateTimeFormatter));
+        };
+    }
+}
--- src/main/java/com/example/pruebaApp/controller/AppRestController.java
+++ src/main/java/com/example/pruebaApp/controller/AppRestController.java
@@ -1,6 +1,8 @@
 package com.example.pruebaApp.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,10 +10,18 @@ import com.example.pruebaApp.model.Greeting;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Optional;
+
 @RestController
 public class AppRestController {
 	
+	private static final String URL = null;
 	ObjectMapper objectMapper = new ObjectMapper();
+	@Value("${java.net.URL}")
+	private String url;
 	
 	@GetMapping(path = "/getBar" )
 	public ResponseEntity<JsonNode> getAllBares(){
@@ -26,5 +36,24 @@ public class AppRestController {
 		
 		return ResponseEntity.ok().body(json);		
 	}
+	
+	@GetMapping(path = "/getJson" )
+	public ResponseEntity<JsonNode> leerJson() throws IOException {
+		//String url = System.getenv("JAVA_HOME"); leo variables de entorno
+		//String url = System.getProperty("user.dir"); leo variables de internas de JAVA
+		ObjectMapper objectMapper = new ObjectMapper();
+		System.out.println("Ejecuto el codigo");
+		System.out.println(url);
+        File archivoJson = new File("src/main/resources/static/"+url);
+        Optional<JsonNode>  json = null;
+
+        // Convierte el archivo JSON a un objeto Java
+		json = Optional.ofNullable(objectMapper.readTree(archivoJson));
+		JsonNode jsonNode = json.orElseThrow();
+        System.out.println(jsonNode.toPrettyString());
+		return ResponseEntity.ok().body(jsonNode);
+	}
 
 }
+
+
