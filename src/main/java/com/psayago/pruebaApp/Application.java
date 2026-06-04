c9fd2e7739a2103ebc8f864950a997689520de83
Pablo Sayago <psayago@frba.utn.edu.ar>
2026-06-03 23:19:00 -0300

    add test #3

--- src/main/java/com/psayago/pruebaApp/Application.java
+++ src/main/java/com/psayago/pruebaApp/Application.java
@@ -1,4 +1,4 @@
-package com.example.pruebaApp;
+package com.psayago.pruebaApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/psayago/pruebaApp/JacksonConfiguration.java
+++ src/main/java/com/psayago/pruebaApp/JacksonConfiguration.java
@@ -1,4 +1,4 @@
-package com.example.pruebaApp;
+package com.psayago.pruebaApp;
 
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
